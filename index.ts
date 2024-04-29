c833e1df0b424cf94f6dfbc1d36d791467b4f89b
Zainab Abdul Majeed <zj469909@gmail.com>
2024-04-29 12:56:43 +0500

    Class_Assignment

---
+++ index.ts
@@ -0,0 +1,51 @@
+//Question 1 
+let message="Hello world!";
+console.log(message);
+//Question 2
+let temperature=30;
+if (temperature>=20)
+{
+    console.log("Temperature is cold");
+}
+//Question 3
+let applesnumbers=10;
+let givenapplesnumber=3;
+console.log(`Apples =${givenapplesnumber}`);
+//Question 4 
+let firstname="Zainab";
+let lastname="Javed";
+let fullname=`${firstname}+${lastname}`;
+console.log(fullname);
+//Question 5
+let number=5;
+if(number>3){
+    console.log("yes");
+}else{
+    console.log("no");
+}
+//Question 6
+let radiusofcircle=22/7;
+//QUESTION 7
+let loop=1;
+let x = 50;
+if(x%5==0){
+    console.log("Buzz")
+}else if(x%3==0){
+    console.log("Fizz")
+} else {
+    console.log(x)
+}
+//Question 9
+let age=20;
+if (age<=19) {
+    console.log("They are Minor");
+} else {
+    console.log("They are Adut");
+}
+//Question 10
+const numberArray=[1,2,3,4,5,-6,-7,-8,0,100];
+const checknumberPositive=(numberArray: number[])=>{
+    let count=0;
+    for(let i =0;i>numberArray.length;i++)
+    };
+// Question 11
